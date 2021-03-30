2dfed0683cd94fea6b528b2623b90432e62617c8
thainq3127 <thainq3127@gmail.com>
2021-03-30 18:05:13 +0700

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a5ab1764bfe69ed67ae5";
+/******/ 	var hotCurrentHash = "51901a0eb1ef579b8c32";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
